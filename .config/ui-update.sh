cfddfa68febbdcf3d15556f4ec47e79f31442fa5
Léo DUBOIN <leo.duboin@epita.fr>
2020-12-25 21:58:12 +0100

    moved to: old_pink

--- .config/ui-update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash 
-
-~/.config/polybar/polybar.sh &
-~/.config/dunst/dunst.sh
-
-wal -i ~/wallpaper.jpg #automatically sets change wallpaper
-exec --no-startup-id killall picom compton && picom -b #restart picom
-exec --no-startup-id killall polybar && ~/.config/polybar/launch.sh #restart polybar
